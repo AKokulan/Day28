034e423a77726a4c915f8ea614b4e2143603d176
Kokulan Ampikanithy <kokul1991@gmail>
2021-02-09 05:49:13 +0530

    commit

--- ariweb/settings.py
+++ ariweb/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'ariweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR,'templates')),(os.path.join(BASE_DIR,'users_app/templates'))],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
